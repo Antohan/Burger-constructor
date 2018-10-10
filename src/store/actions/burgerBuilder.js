08e3f35225b68ff69caea91613564dd9f96fddee
Anton Suhodolets <justanton19@gmail.com>
2018-10-10 17:12:29 +0700

    Moving the BurgerBuilder Side Effects into Sagas.

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,7 +1,5 @@
 import * as actionTypes from './actionTypes';
 
-import axios from '../../axios-orders';
-
 export const addIngredient = (name) => {
   return {
     type: actionTypes.ADD_INGREDIENT,
@@ -30,14 +28,7 @@ export const fetchIngredientsFailed = () => {
 };
 
 export const initIngredients = () => {
-  return dispatch => {
-    axios
-      .get('https://react-burger-api.firebaseio.com/ingredients.json')
-      .then(response => {
-        dispatch(setIngredients(response.data));
-      })
-      .catch(error => {
-        dispatch(fetchIngredientsFailed());
-      });
+  return {
+    type: actionTypes.INIT_INGREDIENTS
   };
 };
